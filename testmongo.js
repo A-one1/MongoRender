3c3c2635ad62e5ae1799d27c7ff37a222808c0e9
Aone Shrestha <aoneshrestha58@gmail.com>
2024-02-29 14:59:18 -0600

    changes mongo uri

--- testmongo.js
+++ testmongo.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require("mongodb");
 
 // The uri string must be the connection string for the database (obtained on Atlas).
-const uri = "mongodb+srv://<user>:<password>@ckmdb.5oxvqja.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = "mongodb+srv://test:test@sample415.v6mmlhp.mongodb.net/?retryWrites=true&w=majority&appName=sample415";
+// mongodb+srv://test:test@sample415.v6mmlhp.mongodb.net/?retryWrites=true&w=majority&appName=sample415
 // --- This is the standard stuff to get it to work on the browser
 const express = require('express');
 const app = express();
@@ -35,8 +35,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('sample415');
+    const parts = database.collection('test');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
